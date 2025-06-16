40f87c3d45a5b8e38708048512e575e793648791
Felipe Gibran <ft.gibran@gmail.com>
2025-06-16 08:23:21 -0300

    added data

---
+++ data/AugmentData/Legendary/legendaryAugmentImageMap.ts
@@ -0,0 +1,47 @@
+// This file maps each legendary augment to its Firebase image URL
+// Update this file if you add or rename images in Firebase
+
+export const legendaryAugmentImageMap: { [key: string]: string } = {
+  AdaptiveReflection:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FAdaptiveReflection.PNG?alt=media',
+  ArcaneConduit:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FArcane%20Conduit.PNG?alt=media',
+  Blightfire:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FBlightfire.PNG?alt=media',
+  CelestialCycle:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FCelestial%20Cycle.PNG?alt=media',
+  Chronoguard:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FChronoguard.PNG?alt=media',
+  CorbomiteProtocol:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FCorbomite%20Protocol.PNG?alt=media',
+  EternalHunger:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FEternal%20Hunger.PNG?alt=media',
+  EvasiveVeiling:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FEvasiveVeiling.PNG?alt=media',
+  FinalAbounding:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FFinalAbounding.PNG?alt=media',
+  JaggedEnd:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FJaggedEnd.PNG?alt=media',
+  LeviathansFury:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FLeviathansFury.PNG?alt=media',
+  LifeStorm:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FLifestorm.PNG?alt=media',
+  OmnisourceSurge:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FOmnisourceSurge.PNG?alt=media',
+  PowersAscent:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FPowersAscent.PNG?alt=media',
+  RetributionCall:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FRetributionCall.PNG?alt=media',
+  RuinationPrism:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FRuination%20Prism.PNG?alt=media',
+  TimesRespite:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FTimeRespite.PNG?alt=media',
+  TitansRedoubt:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FTitansRedoubt.PNG?alt=media',
+  Velthrax:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FVelthrax.PNG?alt=media',
+  Voidcleaver:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FVoidcleaver.PNG?alt=media',
+  RetributionsCall:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FRetributionCall.PNG?alt=media',
+}
---
+++ data/AugmentData/Synergy/synergyImprintImageMap.ts
@@ -0,0 +1,72 @@
+// This file maps each synergy imprint to its Firebase image URL
+// Update this file if you add or rename images in Firebase
+
+export const synergyImprintImageMap: { [key: string]: string } = {
+  // Base Imprints
+  AegisImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_AegisImprint.PNG?alt=media',
+  AirImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Air.PNG?alt=media',
+  ArcaniteImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_ArcaniteImprint.PNG?alt=media',
+  BehemothImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_BehemotImprint.PNG?alt=media',
+  BerserkerImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_BerserkerImprint.PNG?alt=media',
+  BulwarkImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_BulwarkImprint.PNG?alt=media',
+  ColossusImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_ColossusImprint.PNG?alt=media',
+  EmpathImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_EmpathImprint.PNG?alt=media',
+  EnchanterImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_EnchanterImprint.PNG?alt=media',
+  FighterImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_FighterImprint.PNG?alt=media',
+  InvokerImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_InvokerImprint.PNG?alt=media',
+  MysticImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_MysticImprint.PNG?alt=media',
+  PsionImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_PsionImprint.PNG?alt=media',
+  RogueImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_RogueImprint.PNG?alt=media',
+  VanguardImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_VanguardImprint.PNG?alt=media',
+
+  // Composite Imprints
+  BloomImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Bloom.PNG?alt=media',
+  DustImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Dust.PNG?alt=media',
+  FrostImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_FrostAffinity.PNG?alt=media',
+  GraniteImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_GraniteAffinity.PNG?alt=media',
+  HarbingerImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Harbinger.PNG?alt=media',
+  InfernoImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Inferno.PNG?alt=media',
+  MudImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Mud.PNG?alt=media',
+  PhantomImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Phantom.PNG?alt=media',
+  PredatorImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Predator.PNG?alt=media',
+  RevenantImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Revenant.PNG?alt=media',
+  ShockImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Shock.PNG?alt=media',
+  SlayerImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Slayer.PNG?alt=media',
+  SporeImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Spore.PNG?alt=media',
+  TempestImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Tempest.PNG?alt=media',
+  TemplarImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Templar.PNG?alt=media',
+  ToxicImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Toxic.PNG?alt=media',
+  TsunamiImprint:
+    'https://firebasestorage.googleapis.com/v0/b/illuvilytics.firebasestorage.app/o/legendaryaugmentsandimprints%2FT_LegendaryAugment_Tsunami.PNG?alt=media',
+}
