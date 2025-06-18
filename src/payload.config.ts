e3f1d7245846fee1842c7bc880d136403b21a9e4
Felipe Gibran <ft.gibran@gmail.com>
2025-06-18 13:38:17 -0300

    added dependency: @payloadcms/storage-vercel-blob v3.42.0 to package.json and updated pnpm-lock.yaml

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import path from 'path'
 import { buildConfig, PayloadRequest } from 'payload'
 import sharp from 'sharp' // sharp-import
@@ -81,7 +82,13 @@ export default buildConfig({
   globals: [Header, Footer],
   plugins: [
     ...plugins,
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
